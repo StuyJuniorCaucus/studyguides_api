b82bfbc32732b205a2935bf04ce86af5550f460e
Victor Veytsman <vityavv@gmail.com>
2020-12-21 16:18:06 -0500

    Add approve page and main page + backend code

--- db.js
+++ db.js
@@ -8,19 +8,27 @@ const client = new Client({
 })
 const init = async () => {
 	await client.connect()
-	await client.query("CREATE TABLE IF NOT EXISTS studyguides (id SERIAL PRIMARY KEY, submittedBy TEXT, title TEXT, subject TEXT, approved BOOLEAN, link TEXT)")
+	await client.query("CREATE TABLE IF NOT EXISTS studyguides (id SERIAL PRIMARY KEY, submittedBy TEXT, title TEXT, subject TEXT, approved BOOLEAN, link TEXT, email TEXT)")
 	console.log("DB initialized")
 }
 
-const newGuide = async ({submittedBy, title, subject, link}) => {
-	if (!submittedBy || !title || !subject || !link) throw new Error("One or more of the parameters is missing!")
-	await client.query("INSERT INTO studyguides(submittedBy, title, subject, link, approved) VALUES ($1, $2, $3, $4, FALSE)", [submittedBy, title, subject, link])
+const newGuide = async ({submittedBy, title, subject, link, email}) => {
+	if (!submittedBy || !title || !subject || !link || !email) throw new Error("One or more of the parameters is missing!")
+	await client.query("INSERT INTO studyguides(submittedBy, title, subject, link, email, approved) VALUES ($1, $2, $3, $4, $5, FALSE)", [submittedBy, title, subject, link, email])
 }
 const getGuides = async () => {
-	return await client.query("SELECT * FROM studyguides WHERE approved=TRUE")
+	const res = await client.query("SELECT * FROM studyguides WHERE approved=TRUE")
+	return res.rows
+}
+const allGuides = async () => {
+	const res = await client.query("SELECT * FROM studyguides")
+	return res.rows
 }
 const approveGuide = async id => {
-	await client.query("UPDATE TABLE studyguides SET approved=TRUE WHERE id=$1", [id])
+	await client.query("UPDATE studyguides SET approved=TRUE WHERE id=$1", [id])
+}
+const deleteGuide = async id => {
+	await client.query("DELETE FROM studyguides WHERE id=$1", [id])
 }
 
-export {init, newGuide, getGuides, approveGuide}
+export {init, newGuide, getGuides, allGuides, approveGuide, deleteGuide}
--- index.js
+++ index.js
@@ -1,12 +1,50 @@
+//the following two are required to replace __dirname because of ESM modules
+import {dirname} from "path"
+import {fileURLToPath} from "url"
+
 import express from "express"
 const app = express()
+app.set("view engine", "ejs")
+app.use(express.urlencoded())
 const port = process.env.PORT || 8080
 
-import {init, newGuide, getGuides, approveGuide} from "./db.js"
+import {init, newGuide, getGuides, allGuides, approveGuide, deleteGuide} from "./db.js"
 await init()
 
 app.get("/", (req, res) => {
-	res.send("Hello, world!")
+	res.sendFile("views/index.html", {root: dirname(fileURLToPath(import.meta.url))})
+})
+app.get("/guides", async (req, res) => {
+	res.json(await getGuides())
+})
+app.get("/approve", async (req, res) => {
+	res.render("approve.ejs", {guides: await allGuides()})
+})
+app.post("/approve", async (req, res) => {
+	if (req.body.code !== (process.env.CODE || "test")) {
+		res.send("Invalid code!")
+		return
+	}
+	Object.entries(req.body).forEach(e => {
+		if (e[0] === "code" || e[1] !== "on" || !/(?:delete|approve)\d+/.test(e[0])) return
+		if (e[0].startsWith("delete")) deleteGuide(Number(e[0].slice("delete".length)))
+		if (e[0].startsWith("approve")) approveGuide(Number(e[0].slice("approve".length)))
+	})
+	res.redirect("/approve")
+})
+app.post("/submit", async (req, res) => {
+	if (!req.body.submittedBy || !req.body.title || !req.body.subject || !req.body.link || !req.body.email) {
+		res.send(400, "One or more parameters is missing!")
+		return
+	}
+	await newGuide({
+		submittedBy: req.body.submittedBy,
+		title: req.body.title,
+		subject: req.body.subject,
+		link: req.body.link,
+		email: req.body.email
+	})
+	res.redirect("https://stuyjuniorcaucus.github.io/study_guides.html")
 })
 
 app.listen(port, () => console.log("Started"))
